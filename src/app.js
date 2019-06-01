494ceb8237c898960a560fad235ac98eb119437c
Xochil Squaglia <xochilsquaglia@yahoo.com>
2019-06-01 13:37:05 -0700

    adding the backend

---
+++ src/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//3rd Party Resources
+const express = require('express');
+const cors = require('cors');
+const router = require('./router/routes');
+
+//Prepare express app
+const app = express();
+
+//App level MW
+app.use(cors());
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(router);
+
+let isRunning = false;
+
+module.exports = {
+  server: app,
+  start: (port) => {
+    if( ! isRunning ) {
+      app.listen(port, () => {
+        isRunning = true;
+        console.log(`Server Up on ${port}`);
+      });
+    }
+    else {
+      console.log('Server is already running');
+    }
+  }
+};
---
+++ src/router/routes.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const Model = require('../models/score-model');
+const model = new Model();
+const path = require('path');
+const fs = require('fs');
+
+const textToSpeech = require('../modules/dictionary');
+
+//============================================================
+// Router
+//============================================================
+
+router.get('/', (request, response) => {
+  response.render('index');
+});
+router.post('/game', startGame);
+router.post('/score', postScore);
+router.get('/scores', getScores);
+
+//============================================================
+// Functionality
+//============================================================
+
+// Ask user name, difficulty, # words
+async function startGame(request, response, next){
+  let name = request.body.name;
+  let difficulty = request.body.difficulty;
+  let number = request.body.number;
+
+  let directory = `${__dirname}/../../public/audio`;
+
+  fs.readdir(directory, (err, files) => {
+    if (err) console.log(err);
+
+    if (files){
+      for (const file of files) {
+        fs.unlink(path.join(directory, file), err => {
+          console.log('Deleting: ' + file);
+          if (err) console.log(err);
+        });
+      }
+    }
+  });
+
+  try {
+    let words = await textToSpeech(number, difficulty);
+    response.status(200).render('game', {name: name, difficulty: difficulty, number: number, words: words});
+  } catch(e) {
+    next(e);
+  }
+}
+
+function postScore(request, response, next){
+  model.post(request.body)
+    .then(result => {
+      response.status(200).render('score', {score: result})
+    })
+    .catch(next);
+}
+
+function getScores(request, response, next){
+  model.get()
+    .then(result => {
+      response.status(200).render('scores', {scores: result})
+    })
+    .catch(next);
+}
+
+module.exports = router;
